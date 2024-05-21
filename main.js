13eb2c830af382b078999f7645879343e29843aa
pairmaster <pairaraone@gmail.com>
2024-05-21 19:47:59 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const querySelector=document.querySelector("#교사")
+document.querySelector("#일자")
+document.querySelector("#학급")
+const button=document.querySelector("#시간표 > table > tbody > tr:nth-child(1) > td:nth-child(1) > input[type=button]")
+document.querySelector("#시간표 > table > tbody > tr:nth-child(1) > td:nth-child(3) > input[type=button]")
+
